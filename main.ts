3bfe25dd54e4e395aa8a36f4e13cc1287d6fb994
HafsaTahirAmin <161033923+HafsaTahirAmin@users.noreply.github.com>
2024-05-25 21:45:24 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,19 +1,5 @@
-//  Q: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
-// If you don’t have anything specific to write because your programs are too simple at this point, just add your name
-// and the current date at the top of each program file.
-// Then write one sentence describing what the program does.
+// Q: Personal Message: Store a person’s name in a variable, and print a message to that person.
+// Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 
-//  Author: "Hafsa Tahir"
-//  Date: "21 May 2024"
-// Storing the person name in a variable.
 let personName : string= "HafsaTahir";
-// Printing a message to the person.
-console.log ("Hello", (personName), 'would you like to learn some typescript today?' );
-
-
-//  Author: "Hafsa Tahir"
-//  Date: "21 May 2024"
-// Storing my favourite number in a variable
-let favouriteNumber= 31;
-// Printing my favourite number in a message format.
-console.log("My Favourite Number is", favouriteNumber);
+console.log ("Hello", (personName), 'would you like to learn some typescript today?');
