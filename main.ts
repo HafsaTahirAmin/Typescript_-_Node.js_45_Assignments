c2bd15a305068be5024d01b7d724e3b245ac87a0
HafsaTahirAmin <161033923+HafsaTahirAmin@users.noreply.github.com>
2024-05-25 21:50:59 +0500

    Add files via upload

--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
-// Q: Favorite Number: Store your favorite number in a variable. Then, using that variable,
-// create a message that reveals your favorite number. Print that message.
+//  Q: Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each.
+// If you don’t have anything specific to write because your programs are too simple at this point, just add your name
+// and the current date at the top of each program file.
+// Then write one sentence describing what the program does.
+
+//  Author: "Hafsa Tahir"
+//  Date: "21 May 2024"
+// Storing the person name in a variable.
+let personName : string= "HafsaTahir";
+// Printing a message to the person.
+console.log ("Hello", (personName), 'would you like to learn some typescript today?' );
 
 
+//  Author: "Hafsa Tahir"
+//  Date: "21 May 2024"
+// Storing my favourite number in a variable
 let favouriteNumber= 31;
+// Printing my favourite number in a message format.
 console.log("My Favourite Number is", favouriteNumber);
