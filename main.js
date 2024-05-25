cb57d600943c8cd8fd4b4ebd139e89a7d1d40ba1
HafsaTahirAmin <161033923+HafsaTahirAmin@users.noreply.github.com>
2024-05-25 21:47:29 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 "use strict";
-// Q: Famous Quote: Find a quote from a famous person you admire.
-// Print the quote and the name of its author. Your output should look something like the following,
-// including the quotation marks:
-console.log("Steve Jobs once said", '"The only way to do great work is to love what you do"');
+// Q: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+// Then compose your message and store it in a new variable called message. Print your message.
+let famousPerson = "Steve Jobs";
+let message = "once said, 'The only way to do great work is to love what you do'";
+console.log(famousPerson, message);
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-// Q: Famous Quote: Find a quote from a famous person you admire.
-// Print the quote and the name of its author. Your output should look something like the following,
-// including the quotation marks:
+// Q: Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called famous_person.
+// Then compose your message and store it in a new variable called message. Print your message.
 
-console.log("Steve Jobs once said", '"The only way to do great work is to love what you do"');
-
+let famousPerson= "Steve Jobs";
+let message= "once said, 'The only way to do great work is to love what you do'";
+console.log(famousPerson, message);
